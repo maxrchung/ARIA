d03a05d6952722401eb92c6f4a4884b0144c70dc
verythinice <quake.grizzly@gmail.com>
2015-09-16 16:32:08 -0700

    Boat Controller started

---
+++ Aria/Assets/Scripts/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatController : MonoBehaviour {
+	public float rotationSpeed;
+	public float acceleration;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		if (Input.GetAxis ("Vertical") > 0) {
+			gameObject.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector2 (0, acceleration*Input.GetAxis ("Vertical")));
+		}
+		if (Input.GetAxis ("Horizontal") != 0) {
+			gameObject.GetComponent<Rigidbody2D>().AddTorque(Input.GetAxis ("Horizontal"));
+		}
+	}
+}
