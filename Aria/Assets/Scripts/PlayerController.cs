3775396748653c263c46dc8399ad15545abb8f5e
Nina V <nvolkmut@uci.edu>
2015-09-16 14:50:57 -0700

    Camera script - follows target

---
+++ Aria/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			this.transform.position = new Vector2((float) (transform.position.x + .01), (float) transform.position.y);
+		}
+	}
+}
