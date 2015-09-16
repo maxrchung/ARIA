08f575cb3dd09cd37ba5ef885231f71ef6a03958
Nina V <nvolkmut@uci.edu>
2015-09-16 15:45:07 -0700

    Finish line script implemented

--- Aria/Assets/Scripts/PlayerControllerTemp.cs
+++ Aria/Assets/Scripts/PlayerControllerTemp.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerControllerTemp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
