476e7410dbb3acc644af121d2e449af8aec57e22
Nina V <nvolkmut@uci.edu>
2015-09-17 15:01:44 -0700

    Added boat and driver prefab Started Timer

---
+++ Aria/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private float totalTime;
+	private bool gameStart;
+
+	// Use this for initialization
+	void Start () {
+		totalTime = 0;
+		gameStart = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		totalTime += Time.deltaTime;
+		if(!gameStart) {
+			CountDown();
+		}
+	}
+
+	void CountDown() {
+
+	}
+}
