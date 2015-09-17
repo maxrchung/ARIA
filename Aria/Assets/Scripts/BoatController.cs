063fe9ae5878a0fe21844288e0725d6d19f8208a
Nina V <nvolkmut@uci.edu>
2015-09-17 09:18:28 -0700

    Added controls for separate players

--- Aria/Assets/Scripts/BoatController.cs
+++ Aria/Assets/Scripts/BoatController.cs
@@ -5,18 +5,33 @@ public class BoatController : MonoBehaviour {
 	public float rotationSpeed;
 	public float acceleration;
 
+	public string upKey;
+	public string leftKey;
+	public string rightKey;
+	private int horizontalDir;
+
 	// Use this for initialization
 	void Start () {
-	
+		horizontalDir = 0;
 	}
 
 	void FixedUpdate () {
-		if (Input.GetAxis ("Vertical") > 0) {
-			gameObject.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector2 (0, acceleration*Input.GetAxis ("Vertical")));
+		if(Input.GetKey(leftKey)) {
+			horizontalDir = -1;
 		}
-		if (Input.GetAxis ("Horizontal") != 0) {
+		else if(Input.GetKey(rightKey)) {
+			horizontalDir = 1;
+		}
+		else {
+			horizontalDir = 0;
+		}
+
+		if (Input.GetKey(upKey)) {
+			gameObject.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector2 (0, acceleration));
+		}
+		if (horizontalDir != 0) {
 			float force = Mathf.Sqrt(2*(Mathf.Pow(gameObject.GetComponent<Rigidbody2D>().velocity.magnitude,2F))-2*(Mathf.Pow(gameObject.GetComponent<Rigidbody2D>().velocity.magnitude,2F))*Mathf.Cos((Mathf.PI/4)*Input.GetAxis ("Horizontal")));
-			float angle = (Mathf.PI-(Mathf.PI/4)*Input.GetAxis ("Horizontal"))/2;
+			float angle = (Mathf.PI-(Mathf.PI/4)*horizontalDir)/2;
 			gameObject.GetComponent<Rigidbody2D>().AddForceAtPosition(-transform.TransformVector(new Vector2 (Mathf.Cos (angle)*force,/*-Mathf.Sin(angle)*force*/0)), transform.localPosition);
 		}
 		print (new Vector2 (-transform.InverseTransformVector(gameObject.GetComponent<Rigidbody2D>().velocity).x, 0));
