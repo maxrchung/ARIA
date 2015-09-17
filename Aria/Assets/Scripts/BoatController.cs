696ee30ba0cec49d5e6e536d789be5551c10d887
verythinice <quake.grizzly@gmail.com>
2015-09-17 14:49:30 -0700

    Added backwards movement

--- Aria/Assets/Scripts/BoatController.cs
+++ Aria/Assets/Scripts/BoatController.cs
@@ -14,15 +14,16 @@ public class BoatController : MonoBehaviour {
 		if (Input.GetAxis ("Vertical") > 0) {
 			gameObject.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector2 (0, acceleration*Input.GetAxis ("Vertical")));
 		}
+		else if (Input.GetAxis ("Vertical")<0){
+			gameObject.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector2 (0, acceleration*Input.GetAxis ("Vertical")/2));
+		}
 		if (Input.GetAxis ("Horizontal") != 0) {
 			float force = Mathf.Sqrt(2*(Mathf.Pow(gameObject.GetComponent<Rigidbody2D>().velocity.magnitude,2F))-2*(Mathf.Pow(gameObject.GetComponent<Rigidbody2D>().velocity.magnitude,2F))*Mathf.Cos((Mathf.PI/4)*Input.GetAxis ("Horizontal")));
 			float angle = (Mathf.PI-(Mathf.PI/4)*Input.GetAxis ("Horizontal"))/2;
 			gameObject.GetComponent<Rigidbody2D>().AddForceAtPosition(-transform.TransformVector(new Vector2 (Mathf.Cos (angle)*force,/*-Mathf.Sin(angle)*force*/0)), transform.localPosition);
 		}
-		print (new Vector2 (-transform.InverseTransformVector(gameObject.GetComponent<Rigidbody2D>().velocity).x, 0));
 		if (transform.InverseTransformVector(gameObject.GetComponent<Rigidbody2D>().velocity).x!=0){
 			Vector2 force = transform.InverseTransformVector(gameObject.GetComponent<Rigidbody2D>().velocity);
-			print (force.x);
 			gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(-new Vector2 (force.x, 0));
 		}
 	}
