264cce4b6f4c7154ca66bb96036a201fa2e20440
verythinice <quake.grizzly@gmail.com>
2015-09-18 20:40:53 -0700

    Fixed the particles

--- Aria/Assets/Scripts/GoalScript.cs
+++ Aria/Assets/Scripts/GoalScript.cs
@@ -5,19 +5,26 @@ public class GoalScript : MonoBehaviour {
 	public int team;
 	private GameObject mainCamera;
 	private CameraShakeScript shake;
+	private bool scoreable;
+	private ParticleSystem particles;
 	// Use this for initialization
 	void Start () {
 		mainCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0];
 		shake = mainCamera.GetComponent<CameraShakeScript>();
+		particles = transform.GetChild (0).GetComponent<ParticleSystem>();
+		scoreable=true;
 	}
 	
 	// Update is called once per frame
 	void OnTriggerEnter2D (Collider2D other) {
-		if (other.gameObject.transform.tag.Equals("Cat")){
+		if (other.gameObject.transform.tag.Equals("Cat") && scoreable){
 			print (":(");
+			scoreable=false;
 			shake.screenSlam(0.2f,1.0f);
 			StartCoroutine (Wait (4.0f));
-			transform.GetChild (0).GetComponent<ParticleSystem>().Play();
+			if (!particles.isPlaying){
+				particles.Play();
+			}
 			//Change Score
 			mainCamera.GetComponent<GameManager>().AddScore(team);
 		}
@@ -28,5 +35,9 @@ public class GoalScript : MonoBehaviour {
 		yield return new WaitForSeconds(seconds);
 		//Reset Players
 		mainCamera.GetComponent<GameManager>().NewRound();
+		if (!particles.isStopped){
+			particles.Stop();
+		}
+		scoreable=true;
 	}
 }
